40377751cceb6c3a9fe9ff48074865cd91b1cacc
pkviet <pkv.stream@gmail.com>
2017-10-03 23:57:10 +0200

    fix faulty archive
    replaced the tar.gz which had bad hierarchy of files

--- Library/Formula/decklink.rb
+++ Library/Formula/decklink.rb
@@ -2,7 +2,7 @@ class Decklink < Formula
   desc "decklink sdk"
   homepage "https://github.com/pkviet/ffmpeg-decklink"
   url "https://github.com/pkviet/ffmpeg-decklink/raw/master/decklink-api-10.9.5.tar.gz"
-  sha256 "bbe2b80c5bc9f3499087db2ad2360ca957213944c125767b1ea86e4c4da88601"
+  sha256 "aff1d5e1819f01a4f92da710498487b3d9ffd9d51a9d1a9ed2df6bcdd5e9c8b2"
 
   def install
     include.install Dir["include/*"]
